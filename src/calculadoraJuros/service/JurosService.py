835bbc15dc6899c82d03012687b45f3db232f4dc
AndreMachna <amachna@bairru.com.br>
2023-01-30 16:02:41 -0300

    correção na equação de calculo de juros

--- src/calculadoraJuros/service/JurosService.py
+++ src/calculadoraJuros/service/JurosService.py
@@ -2,25 +2,18 @@ class JurosService():
 
     
     def converte_taxa_mensal_para_anual(self,taxa):
-        taxa = (1 + taxa) ** (1/12) - 1
+        taxa = (1 + taxa)**(12) - 1
         return taxa
     
     def converte_taxa_anual_para_mensal(self,taxa):
-        taxa = ((1 + taxa) ** 12 ) - 1
+        taxa = (1 + taxa)**(1/12) - 1
         return taxa
     
-
-    #Serviço que faz o calculo de juros simples
-    def calcula_juros_simples(self,valor_presente,taxa,tempo):
-        
-        valor_futuro = valor_presente + valor_presente*taxa*tempo
-
-        return float(format(valor_futuro,'.2f'))
-
     #Serviço que faz o calculo de juros compostos
     def calcula_juros_compostos(self,valor_inicial,valor_mensal,taxa,periodo,periodo_em,taxa_em):
         
         taxa = taxa/100
+        print(taxa)
 
         if periodo_em.upper() == 'ANOS':
             if taxa_em.upper() == 'MENSAL':
@@ -29,7 +22,8 @@ class JurosService():
                 taxa_parcela_1 = taxa    
         elif periodo_em.upper() == 'MESES':
             if taxa_em.upper() == 'ANUAL':
-                taxa_parcela_1 = self.converte_taxa_anual_para_mensal(taxa)       
+                taxa_parcela_1 = self.converte_taxa_anual_para_mensal(taxa)
+                print(taxa_parcela_1)       
             else:
                 taxa_parcela_1 = taxa 
         parcela_1 = valor_inicial*(1+taxa_parcela_1)**periodo
@@ -47,3 +41,11 @@ class JurosService():
         juros = montante_final - valor_total_aportado
 
         return montante_final, valor_total_aportado, juros
+
+    #Serviço que faz o calculo de juros simples
+    def calcula_juros_simples(self,valor_presente,taxa,tempo):
+        
+        valor_futuro = valor_presente + valor_presente*taxa*tempo
+
+        return float(format(valor_futuro,'.2f'))
+
